4823305082482b5e0a35be81a2a78f74a5b02d8a
Thomas <thomas.ed.dick@gmail.com>
2022-08-22 20:42:49 -0600

    add favicon and basic site

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,17 +13,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-// app.use(express.favicon("public/images/favicon.ico"));
+app.use("/public", express.static('public')); 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/public/images/favicon.ico', (req, res) => res.status(200))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
