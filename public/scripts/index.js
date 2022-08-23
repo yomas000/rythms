7ff6a30344e1e0e1aba6abc44428a8e492baf2ca
Thomas Dick <thomas.ed.dick@gmail.com>
2022-08-23 15:24:58 -0600

    add stiff

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,3 +7,34 @@ function changeIcon(e){
         img.src = "http://localhost/public/images/play.png"
     }
 }
+
+
+function makePost(){
+    let context;
+    let request;
+    let source;
+
+    try {
+        context = new AudioContext();
+        request = new XMLHttpRequest();
+        request.open("GET", "/audio", true);
+        request.responseType = "arraybuffer";
+
+        request.onload = () => {
+            context.decodeAudioData(request.response, (buffer) => {
+                source = context.createBufferSource();
+                source.buffer = buffer;
+                source.connect(context.destination);
+                // auto play
+                source.start(0); // start was previously noteOn
+
+                
+            });
+        };
+
+        request.send();
+
+    } catch (e) {
+        alert('web audio api not supported');
+    }
+}
